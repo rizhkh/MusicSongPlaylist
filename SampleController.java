a77500f40da15b6b04696fb90548c1e9a31b77d0
Riz <mrk_rizwan@hotmail.com>
2018-09-26 23:20:28 -0400

    Update SampleController.java

--- SampleController.java
+++ SampleController.java
@@ -5,6 +5,9 @@ import java.awt.TextComponent;
 import java.awt.TextField;
 import javafx.event.ActionEvent;
 import java.util.Optional;
+
+import javax.swing.event.ChangeListener;
+
 import javafx.scene.control.Label;
 
 import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Text;
@@ -21,31 +24,72 @@ import javafx.scene.control.*;
 public class SampleController {
 	
     @FXML private Text actiontarget;    
+    //@FXML private  TextField p1;
+    //@FXML TextField textField;
     
     //WHEN YOU PRESS THE ADD BUTTON YOU GET AN ALERT
     @FXML protected void sayHelloWorld(ActionEvent event) {
+    	//p1.SetVisible(True);
+    	/*
     	Alert alert = new Alert(AlertType.INFORMATION);
     	alert.setTitle("Information Dialog");
     	alert.setHeaderText("Look, an Information Dialog");
     	alert.setContentText("I have a great message for you!");
     	alert.showAndWait();
+    	*/
+    	//System.out.println("hello world");
     	}
 
 	@FXML ListView<String> listView;
 	@FXML ListView<String> detail;
 
-
 	private ObservableList<String> obsList;
+	private ObservableList<String> obsList2;
 
+	//String[] g = new String[100];
+	String[] g = {"hey","hey","hey","hey","hey","hey","hey","hey"};
+	//Add a datastructure that would reorganize the string array then show the array
+	//next step is : whenever I click on any item in the list it shows
+	//much more detail in the detail list box
+	
+	///Details - list action
+	//name
+	//artist
+	//year
+	//genre
+	
 	
 	public void start(Stage mainStage)
-	{
-		obsList = FXCollections.observableArrayList("Africa by Toto",
-				"Splunk by R",
-				"Yoyo by Ma"
+	{    
+
+		//THIS PART SHOWS WHATS IN THE DETAIL LIST
+		obsList = FXCollections.observableArrayList(g
 				);
 		listView.setItems(obsList); 
 		
+		/*
+		listView.getSelectionModel().selectedItemProperty().addListener(
+				new ChangeListener<String>()
+		{
+          public void changed(ObservableValue<? extends String> ov,
+              String old_val, String new_val) {
+            label.setText(new_val);
+            label.setTextFill(Color.web(new_val));
+          }
+        });
+		*/
+		
+		//FIND A WAY SOMEHOW WHEN YOU CLICK ON ANY SONG IN LIST IT DISPLAYS INFO OF IT
+		// IN DETAIL
+		
+		//THIS PART SHOWS WHATS IN THE DETAIL LIST
+		obsList2 = FXCollections.observableArrayList(
+				"Name of the song",
+				"Name of the Artist",
+				"Album name",
+				"Release date"
+				);
+		detail.setItems(obsList2);
 		// select the first item
 	      listView.getSelectionModel().select(0);
 
